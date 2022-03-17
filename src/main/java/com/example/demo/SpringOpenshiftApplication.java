810278b7895ff0d33ccb32a88494b899e23c9678
RAVI GUPTA <0011223344ravi@gmail.com>
2022-03-17 09:28:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringOpenshiftApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+public class SpringOpenshiftApplication {
+
+	@GetMapping("/")
+	public String welcome() {
+		
+		return "Welcome to Ravi adda";
+	}
+	
+	@GetMapping("/{input}")
+	public String welcome1(@PathVariable String input) {
+		
+		return "Hi"+ input +"Welcome to Ravi adda";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenshiftApplication.class, args);
+	}
+
+}
