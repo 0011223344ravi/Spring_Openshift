606050bbfea31c6b9cff92cf9a815d9ce8b98f47
RAVI GUPTA <0011223344ravi@gmail.com>
2022-03-23 10:26:16 +0530

    second commit

--- src/main/java/com/example/demo/SpringOpenshiftApplication.java
+++ src/main/java/com/example/demo/SpringOpenshiftApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+//
 @SpringBootApplication
 public class SpringOpenshiftApplication {
 
